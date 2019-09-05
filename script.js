cfae04c79f6951d92119ae72becafb711a411157
Jeff Gipson <mkenzie87@hotmail.com>
2019-09-05 13:21:54 -0500

    Small Changes

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 console.log("Hello");
+// https://www.evocationcoffee.com/pages/ratio
+
+
+
 
 var inverseFactor = '0.0625'; // if measuring in metric
 
-var strength = '';
+var strength = 16;
+
+
+
+
+function coffeeGrams(weight) {
+    var coffeeWeightInput = document.getElementById('coffee-weight').value;
+
+    return weight * strength;
+}
